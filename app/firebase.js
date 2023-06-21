1bfa2ce47857c0543bd2a20e98f6495ca3ff4cd3
Masab Bin Zia <masabmbz5@gmail.com>
2023-06-21 07:34:31 +0500

    09

--- app/firebase.js
+++ app/firebase.js
@@ -8,7 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
- 
+  apiKey: YOUR_API_KEY,
+  authDomain:YOUR_AUTH_DOMAIN ,
+  projectId:YOUR_PROJECT_ID ,
+  storageBucket:YOUR_STORAGE_BUCKET ,
+  messagingSenderId:YOUR_MESSAGING_SENDER_ID,
+  appId: 
 };
 
 // Initialize Firebase
