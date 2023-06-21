3e5314b86ee8f52d10e3ecbfbf85f34db3ccfad0
Masab Bin Zia <masabmbz5@gmail.com>
2023-06-21 07:31:39 +0500

    08

--- app/firebase.js
+++ app/firebase.js
@@ -8,12 +8,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCn2fvWKVnMs0nYS7JfOdWQVN04sTcwtgM",
-  authDomain: "expense-tracker-68474.firebaseapp.com",
-  projectId: "expense-tracker-68474",
-  storageBucket: "expense-tracker-68474.appspot.com",
-  messagingSenderId: "920919766157",
-  appId: "1:920919766157:web:8838876e668e7282eb9ec4"
+ 
 };
 
 // Initialize Firebase
